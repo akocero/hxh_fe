e0382b2fb980c332d25866a71f09aafff9fd5393
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2022-04-25 22:54:21 +0800

    feat: use link instead of import on fonts

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,22 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
 	return (
 		<Html>
-			<Head></Head>
+			<Head>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link
+					rel="preconnect"
+					href="https://fonts.gstatic.com"
+					crossorigin
+				/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Paytone+One&display=swap"
+					rel="stylesheet"
+				/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;500;600;700;800&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
