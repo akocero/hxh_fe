d300b249a598f7c4fc4e0e1ba2b2ac585d8a64b2
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2022-04-28 22:19:59 +0800

    fix: cross origin error on link

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ export default function Document() {
 				<link
 					rel="preconnect"
 					href="https://fonts.gstatic.com"
-					crossorigin
+					crossOrigin="true"
 				/>
 				<link
 					href="https://fonts.googleapis.com/css2?family=Paytone+One&display=swap"
