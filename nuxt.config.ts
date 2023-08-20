55b5679aba06a209f8cc7a5aa793c9858a72efe0
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2023-08-20 18:21:28 +0800

    try this fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	devtools: { enabled: true },
-	modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', 'nuxt-icon']
+	devtools: { enabled: false },
+	modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', 'nuxt-icon'],
+	routeRules: {
+		// Homepage pre-rendered at build time
+		'/': { prerender: true }
+	}
 });
